9a0ee29615f6f7204694d26d92be26c4b06889c3
Paritosh Jha <paritoshjha0504@gmail.com>
2024-09-02 16:11:11 +0530

    Removed unused code from navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -21,7 +21,6 @@ function Navbar() {
     <Container>
       <div className="flex items-center justify-between">
         <NavLink to="/">
-          {/* <img src={Logo} className="h-[50px]" /> */}
           <p className="font-logo text-white text-4xl text-center">Pk.</p>
         </NavLink>
         <div className="flex items-center justify-center lg:gap-x-8 md:gap-x-4 sm:gap-x-2 gap-x-1">
@@ -35,16 +34,3 @@ function Navbar() {
 }
 
 export default Navbar
-
-{
-  /* <NavLink
-      to={path}
-      className={({ isActive }) =>
-        isActive
-          ? 'px-4 py-2 text-sm  rounded bg-white text-black font-bold'
-          : 'px-4 py-2 text-sm text-white font-semibold rounded hover:bg-white hover:text-black'
-      }
-    >
-      {label}
-    </NavLink> */
-}
