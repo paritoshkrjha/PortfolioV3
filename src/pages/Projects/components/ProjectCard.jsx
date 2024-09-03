b53076b2d4d1ac40c978c2768910975cc59e3d46
Paritosh Jha <paritoshjha0504@gmail.com>
2024-09-03 18:57:53 +0530

    Added Projects page

--- src/pages/Projects/components/ProjectCard.jsx
+++ src/pages/Projects/components/ProjectCard.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FiExternalLink } from 'react-icons/fi'
+import { CiFolderOn } from 'react-icons/ci'
+import { PiGithubLogo } from 'react-icons/pi'
+import { motion } from 'framer-motion'
 
-function ProjectCard() {
+function ProjectCard({
+  title,
+  description,
+  projectUrl = '',
+  githubUrl = '',
+  techStack,
+}) {
+  const [gradientPosition, setGradientPosition] = useState({ x: 0, y: 0 })
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleMouseMove = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect()
+    setGradientPosition({
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    })
+  }
+
   return (
-    <div>ProjectCard</div>
+    <motion.div
+      initial={{ y: 0 }}
+      whileHover={{ y: -10, type: 'spring' }}
+      className="relative px-4 py-6 rounded-md bg-[#212131]/60 backdrop-blur-sm overflow-hidden"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && (
+        <div
+          className="absolute inset-0 pointer-events-none"
+          style={{
+            background: `radial-gradient(circle at ${gradientPosition.x}px ${gradientPosition.y}px, rgba(255, 255, 255, 0.1), transparent 20%)`,
+          }}
+        />
+      )}
+      <div className="relative z-10 flex justify-between items-center">
+        <CiFolderOn size={40} color="white" />
+        <div className="flex gap-x-3 cursor-pointer">
+          {projectUrl.length !== 0 && (
+            <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+              <FiExternalLink size={24} color="gray" />
+            </a>
+          )}
+          {githubUrl.length !== 0 && (
+            <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+              <PiGithubLogo size={24} color="gray" />
+            </a>
+          )}
+        </div>
+      </div>
+      <div className="relative z-10 mt-4">
+        <h3 className="text-xl font-bold text-white">{title}</h3>
+        <p className="text-gray-400 mt-2">{description}</p>
+        <div className="flex flex-wrap gap-2 mt-4">
+          {techStack.map((tech, index) => (
+            <span key={index} className="text-sm text-gray-400">
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+
 import Heading from '../../components/Heading'
+import ProjectCard from './components/ProjectCard'
+import AnimatedButton from '../../components/ui/AnimatedButton'
 
 function Projects() {
   return (
     <>
       <Heading label="Projects" />
+      <div className=" mt-5 flex flex-col gap-y-5">
+        <ProjectCard
+          title={'Yumster'}
+          description={
+            ' A social recipe sharing app to connect foodies and explore new dishes.'
+          }
+          githubUrl={'https://github.com/orgs/PrepFront/repositories'}
+          techStack={['Flutter', 'MERN', 'JWT-Auth', 'Google Gemini']}
+        />
+        <ProjectCard
+          title={'JobCadet'}
+          description={
+            'A full stack Web Application to share resources and scheduling DCS and personal mentoring sessions from the app.'
+          }
+          githubUrl={'https://github.com/paritoshkrjha/Yumster'}
+          projectUrl={'https://jobcadet.com/'}
+          techStack={['HTML', 'Tailwind CSS', 'MERN Stack']}
+        />
+        <ProjectCard
+          title={'Eyemate'}
+          description={
+            ' An eye pleasing, easy to navigate and a minimal OTT platform responsive redesign'
+          }
+          projectUrl="https://eyemate.netlify.app/"
+          githubUrl={'https://github.com/paritoshkrjha/eyemate'}
+          techStack={['HTML', 'Tailwind CSS', 'ReactJs']}
+        />
+      </div>
+      <div className="m-8">
+        <AnimatedButton label={'View my Resume'} to="/projects" />
+      </div>
     </>
   )
 }
