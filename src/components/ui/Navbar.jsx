4d6dd010106107d659bc47ffce16db7b8acebcb4
Paritosh Jha <paritoshjha0504@gmail.com>
2024-09-03 14:03:58 +0530

    Added staggered animation to navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,16 +4,29 @@ import { motion } from 'framer-motion'
 
 function NavItem({ label, path }) {
   return (
-    <NavLink
-      to={path}
-      className={({ isActive }) =>
-        isActive
-          ? 'px-4 py-2 text-sm  rounded bg-white text-black font-bold'
-          : 'px-4 py-2 text-sm text-white font-semibold rounded hover:bg-white hover:text-black'
-      }
+    <motion.div
+      variants={{
+        hidden: {
+          y: -40,
+          opacity: 0,
+        },
+        visible: {
+          y: 0,
+          opacity: 1,
+        },
+      }}
     >
-      {label}
-    </NavLink>
+      <NavLink
+        to={path}
+        className={({ isActive }) =>
+          isActive
+            ? 'px-4 py-2 text-sm  rounded bg-white text-black font-bold'
+            : 'px-4 py-2 text-sm text-white font-semibold rounded hover:bg-white hover:text-black'
+        }
+      >
+        {label}
+      </NavLink>
+    </motion.div>
   )
 }
 
@@ -21,24 +34,42 @@ function Navbar() {
   return (
     <Container>
       <motion.div
-        initial={{
-          y: -20,
-          opacity: 0,
+        variants={{
+          hidden: {
+            y: -40,
+            opacity: 0,
+          },
+          visible: {
+            y: 0,
+            opacity: 1,
+          },
         }}
-        animate={{
-          y: 0,
-          opacity: 1,
+        initial="hidden"
+        animate="visible"
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
         }}
         className="flex items-center justify-between"
       >
         <NavLink to="/">
           <p className="font-logo text-white text-4xl text-center">Pk.</p>
         </NavLink>
-        <div className="flex items-center justify-center lg:gap-x-8 md:gap-x-4 sm:gap-x-2 gap-x-1">
+        <motion.div
+          variants={{
+            visible: {
+              transition: {
+                staggerChildren: 0.1,
+              },
+            },
+          }}
+          className="flex items-center justify-center lg:gap-x-8 md:gap-x-4 sm:gap-x-2 gap-x-1"
+        >
           <NavItem label={'About'} path={'/about'} />
           <NavItem label={'Projects'} path={'/projects'} />
           <NavItem label={'Contact'} path={'/contact'} />
-        </div>
+        </motion.div>
       </motion.div>
     </Container>
   )
