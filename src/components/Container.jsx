450ec57f4698d6e6dc01ed9519c97e0cf3f320b1
Paritosh Jha <paritoshjha0504@gmail.com>
2024-09-03 09:28:30 +0530

    Tweaked responsiveness of Container and Background

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Container({ children }) {
   return (
     <div className="py-10 flex items-center justify-center">
-      <div className="lg:w-[50%] md:w-[70%] sm:w-[70%] w-[90%] flex flex-col gap-4">
+      <div className="lg:w-[60%] md:w-[70%] sm:w-[70%] w-[90%] flex flex-col gap-4">
         {children}
       </div>
     </div>
--- src/components/ui/Background.jsx
+++ src/components/ui/Background.jsx
@@ -1,7 +1,7 @@
 function Background({ children }) {
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-      {children}
+    <div className="fixed top-0 left-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <div className="relative h-full w-full overflow-auto">{children}</div>
     </div>
   )
 }
